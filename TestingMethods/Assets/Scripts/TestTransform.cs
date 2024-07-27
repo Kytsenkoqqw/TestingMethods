1c52cef70ecce0a1e1ec4759c41e3c3b201df340
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-07-27 17:01:18 +0300

    Add information panel

---
+++ TestingMethods/Assets/Scripts/TestTransform.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTransform : MonoBehaviour
+{
+    public float Speed = 5f;
+    public Renderer Render;
+    public GameObject Panel;
+    void Start()
+    {
+        Render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Render.material.color = Color.red;
+        Panel.SetActive(true);
+    }
+
+    private void OnMouseOver()
+    {
+        Render.material.color -= new Color(0.1f, 0,0 ) * Time.deltaTime;
+    }
+
+    private void OnMouseExit()
+    {
+        Render.material.color = Color.white;
+        Panel.SetActive(false);
+    }
+}
